3c1ea78650ec1c84ce04ca88a0476daca29e8f81
Sam Orgill <ssorgill@icloud.com>
2018-06-10 16:09:52 +0100

    Added planets

--- AR Pathfinder/ViewController.swift
+++ AR Pathfinder/ViewController.swift
@@ -20,14 +20,109 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+//        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let sun = SCNSphere(radius: 0.5)
+        let mercury = SCNSphere(radius: 0.02)
+        let venus = SCNSphere(radius: 0.05)
+        let earth = SCNSphere(radius: 0.055)
+        let mars = SCNSphere(radius: 0.05)
+        let jupiter = SCNSphere(radius: 0.1)
+        let saturn = SCNSphere(radius: 0.095)
+        let uranus = SCNSphere(radius: 0.08)
+        let neptune = SCNSphere(radius: 0.077)
         
-        // Set the scene to the view
-        sceneView.scene = scene
+        let sunMaterial = SCNMaterial()
+        sunMaterial.diffuse.contents = UIImage(named: "art.scnassets/sun.jpg")
+        
+        let mercuryMaterial = SCNMaterial()
+        mercuryMaterial.diffuse.contents = UIImage(named: "art.scnassets/mercury.jpg")
+        
+        let venusMaterial = SCNMaterial()
+        venusMaterial.diffuse.contents = UIImage(named: "art.scnassets/venus.jpg")
+        
+        let earthMaterial = SCNMaterial()
+        earthMaterial.diffuse.contents = UIImage(named: "art.scnassets/earthday.jpg")
+        
+        let marsMaterial = SCNMaterial()
+        marsMaterial.diffuse.contents = UIImage(named: "art.scnassets/mars.jpg")
+        
+        let jupiterMaterial = SCNMaterial()
+        jupiterMaterial.diffuse.contents = UIImage(named: "art.scnassets/jupiter.jpg")
+        
+        let saturnMaterial = SCNMaterial()
+        saturnMaterial.diffuse.contents = UIImage(named: "art.scnassets/saturn.jpg")
+        
+        let uranusMaterial = SCNMaterial()
+        uranusMaterial.diffuse.contents = UIImage(named: "art.scnassets/uranus.jpg")
+        
+        let neptuneMaterial = SCNMaterial()
+        neptuneMaterial.diffuse.contents = UIImage(named: "art.scnassets/neptune.jpg")
+        
+        sun.materials = [sunMaterial]
+        mercury.materials = [mercuryMaterial]
+        venus.materials = [venusMaterial]
+        earth.materials = [earthMaterial]
+        mars.materials = [marsMaterial]
+        jupiter.materials = [jupiterMaterial]
+        saturn.materials = [saturnMaterial]
+        uranus.materials = [uranusMaterial]
+        neptune.materials = [neptuneMaterial]
+        
+        let sunNode = SCNNode()
+        sunNode.position = SCNVector3(x: 0.1, y: 0.2, z: -3)
+        sunNode.geometry = sun
+
+        let mercuryNode = SCNNode()
+        mercuryNode.position = SCNVector3(x: 0.2, y: 0.2, z: -2)
+        mercuryNode.geometry = mercury
+        
+        let venusNode = SCNNode()
+        venusNode.position = SCNVector3(x: 0.1, y: 0.4, z: -2)
+        venusNode.geometry = venus
+        
+        let earthNode = SCNNode()
+        earthNode.position = SCNVector3(x: 0.3, y: 0.3, z: -1.5)
+        earthNode.geometry = earth
+        
+        let marsNode = SCNNode()
+        marsNode.position = SCNVector3(x: 0.4, y: 0.2, z: -1)
+        marsNode.geometry = mars
+        
+        let jupiterNode = SCNNode()
+        jupiterNode.position = SCNVector3(x: 0.5, y: -0.1, z: -4)
+        jupiterNode.geometry = jupiter
+        
+        let saturnNode = SCNNode()
+        saturnNode.position = SCNVector3(x: 0.6, y: -0.2, z: -5)
+        saturnNode.geometry = saturn
+        
+        let uranusNode = SCNNode()
+        uranusNode.position = SCNVector3(x: 0.75, y: -0.3, z: -4)
+        uranusNode.geometry = uranus
+        
+        let neptuneNode = SCNNode()
+        neptuneNode.position = SCNVector3(x: 0.8, y: -0.4, z: -3)
+        neptuneNode.geometry = neptune
+        
+        sceneView.scene.rootNode.addChildNode(sunNode)
+        sceneView.scene.rootNode.addChildNode(mercuryNode)
+        sceneView.scene.rootNode.addChildNode(venusNode)
+        sceneView.scene.rootNode.addChildNode(earthNode)
+        sceneView.scene.rootNode.addChildNode(marsNode)
+        sceneView.scene.rootNode.addChildNode(jupiterNode)
+        sceneView.scene.rootNode.addChildNode(saturnNode)
+        sceneView.scene.rootNode.addChildNode(uranusNode)
+        sceneView.scene.rootNode.addChildNode(neptuneNode)
+        
+        
+        sceneView.autoenablesDefaultLighting = true
+        
+//        // Create a new scene
+//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+//
+//        // Set the scene to the view
+//        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -45,36 +140,5 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Pause the view's session
         sceneView.session.pause()
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
-
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
     }
 }
